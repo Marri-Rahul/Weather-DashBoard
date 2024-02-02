45c7e093a41d4c6a40df2a220f411eba6b7954e4
MarriRahul295 <115916791+MarriRahul295@users.noreply.github.com>
2024-02-02 19:22:15 +0530

    Files

---
+++ src/js/weatherApp.js
@@ -0,0 +1,90 @@
+import { createHourlyCards, createDailyCards } from "./weatherForecastCards.js";
+import { startLoadingState, endLoadingState } from "./setLoadingState.js";
+import { handleError } from "./handleError.js";
+import { currentWeatherData } from "./currentWeatherData.js";
+import { weatherForecastData } from "./weatherForecastData.js";
+
+// const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY='0bb53bfacf7a859e6c1bd597f79c81d9';
+
+const searchBoxInput = document.querySelector(".search-box-input");
+const gpsButton = document.querySelector(".gps-button");
+const ctaButton = document.querySelector(".cta-button");
+const topButton = document.querySelector(".top-button");
+// s
+// const tempToggleCelsius = document.querySelector('.temperature-toggle-celsius');
+// const tempToggleFahrenheit = document.querySelector('.temperature-toggle-fahrenheit');
+
+// let isCelsius = true;
+
+// tempToggleCelsius.addEventListener('click', () => {
+//   isCelsius = true;
+//   displayWeather();
+// });
+
+// tempToggleFahrenheit.addEventListener('click', () => {
+//   isCelsius = false;
+//   displayWeather(); 
+// });
+// // e
+createHourlyCards();
+createDailyCards();
+
+const fetchWeatherData = async (data) => {
+  try {
+    await startLoadingState();
+    await currentWeatherData(data, API_KEY);
+    await weatherForecastData(data, API_KEY);
+    await endLoadingState();
+  } catch (error) {
+    if (error.message === "Failed to fetch") {
+      await handleError(
+        "Uh oh! It looks like you're not connected to the internet. Please check your connection and try again.",
+        "Refresh Page"
+      );
+    } else {
+      await handleError(error.message, "Try Again");
+    }
+  }
+};
+
+const getUserLocation = async () => {
+  const successCallback = async (position) => {
+    const data = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    };
+
+    await fetchWeatherData(data);
+  };
+
+  const errorCallback = (error) => {
+    console.log(error);
+    fetchWeatherData("Istanbul");
+  };
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+searchBoxInput.addEventListener("keyup", async (event) => {
+  if (event.keyCode === 13) {
+    await fetchWeatherData(searchBoxInput.value);
+  }
+});
+
+gpsButton.addEventListener("click", getUserLocation);
+
+ctaButton.addEventListener("click", () => {
+  window.open("https://github.com/Marri-Rahul/Weather-DashBoard/tree/main");
+});
+
+topButton.addEventListener("click", scrollToTop);
+
+getUserLocation();
